dc62872f6e57ed1df40e1d9aaabf578050a6bd91
yair sabag <yairsabag213@gmail.com>
2022-09-05 14:11:27 +0300

    #shot 3

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = process.env.DB_URL3 || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL4 || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
