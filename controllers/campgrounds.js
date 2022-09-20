def720647b666b3d61a092fb064118b0911de974
yair sabag <yairsabag213@gmail.com>
2022-09-20 09:44:39 +0300

    fix mapbox moudle

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,5 @@
 const Campground = require('../models/campground');
-const mbxGeocoding = require("mapbox-sdk-js/services/geocoding");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const { cloudinary } = require("../cloudinary");
