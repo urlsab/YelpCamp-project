faa65eaca327a1687c291266a93f7dd0b04d54fb
yair sabag <yairsabag213@gmail.com>
2022-09-20 08:57:08 +0300

    add procfile and env vars to heroku settings

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -3,8 +3,8 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const storage = new CloudinaryStorage({
