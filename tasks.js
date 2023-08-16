e8b8bfcc70fb4e1d819bdc4801c9a863f949ec52
yair sabag <yairsabag213@gmail.com>
2023-08-16 16:35:09 +0300

    fix all vulnerabilities

---
+++ tasks.js
@@ -0,0 +1,11 @@
+/*
+
+* fix refused to load script because it violates the following Content-Security-Policy directive
+
+* limit styles for all screen sizes (@max-size etc)
+
+* install mongodb shell new version
+
+* install mongosh shell new version
+
+*/
